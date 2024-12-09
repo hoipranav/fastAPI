92ba05ee2fcc366b9f5bcca72f7de0dbc3ebad0f
hoipranav <hoipranavv@gmail.com>
2024-12-09 16:10:13 +0530

    refactor: remove redundant print statement

--- app/oauth2.py
+++ app/oauth2.py
@@ -48,7 +48,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
                                           detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
     
     token = verify_access_token(token, credentials_exception)
-    print(token)
+    
     user = db.query(models.User).filter(models.User.email == token.id).first()
     
     return user
