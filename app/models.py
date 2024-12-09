d1f9481d9a1e09c5573d00f7d80981e29e8d7b8f
hoipranav <hoipranavv@gmail.com>
2024-12-09 16:09:30 +0530

    feat: add user data fetching functionality

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
 from .database import Base
 
 class Post(Base):
@@ -12,6 +13,9 @@ class Post(Base):
     published = Column(Boolean, server_default='True', nullable=False)    
     created_at = Column(TIMESTAMP(timezone=True), 
                         nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    
+    owner = relationship("User")
     
     
 class User(Base):
